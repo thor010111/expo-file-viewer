3deb1a74589d86e555257f3c3d75eef7a5169bc5
Thorsten Kehren <thorsten.wesseler@johnnybytes.com>
2024-06-26 11:03:02 +0200

    fix: platforms that are not iOS

--- src/ExpoFileViewerModule.ts
+++ src/ExpoFileViewerModule.ts
@@ -1,5 +1,35 @@
-import { requireNativeModule } from 'expo-modules-core';
+import * as FileSystem from "expo-file-system";
+import { startActivityAsync } from "expo-intent-launcher";
+import { requireNativeModule } from "expo-modules-core";
+import { Linking, Platform } from "react-native";
 
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default requireNativeModule('ExpoFileViewer');
+interface ExpoFileViewerModule {
+  openFile(filePath: string): Promise<void>;
+}
+
+let expoFileViewerModule: ExpoFileViewerModule;
+
+switch (Platform.OS) {
+  case "ios":
+    expoFileViewerModule = requireNativeModule("ExpoFileViewer");
+    break;
+  case "android":
+    expoFileViewerModule = {
+      openFile: async (filePath: string) => {
+        const contentUri = await FileSystem.getContentUriAsync(filePath);
+        await startActivityAsync("android.intent.action.VIEW", {
+          data: contentUri,
+          flags: 1,
+        });
+      },
+    };
+    break;
+  default:
+    expoFileViewerModule = {
+      openFile: async (filePath: string) => {
+        await Linking.openURL(filePath);
+      },
+    };
+}
+
+export default expoFileViewerModule;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-import * as FileSystem from "expo-file-system";
-import { startActivityAsync } from "expo-intent-launcher";
-import { Linking, Platform } from "react-native";
-
 import ExpoFileViewerModule from "./ExpoFileViewerModule";
 
-async function openFile(filePath: string) {
-  if (Platform.OS === "web") {
-    await Linking.openURL(filePath);
-  } else if (Platform.OS === "android") {
-    const contentUri = await FileSystem.getContentUriAsync(filePath);
-    await startActivityAsync("android.intent.action.VIEW", {
-      data: contentUri,
-      flags: 1,
-    });
-  } else if (Platform.OS === "ios") {
-    await ExpoFileViewerModule.openFile(filePath);
-  }
-}
-
-export { openFile };
+export default ExpoFileViewerModule;
