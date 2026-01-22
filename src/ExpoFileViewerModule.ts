f5aa2e4c42bb618983d255c48932b690220b2f8e
maya-kilbertus <158475666+maya-kilbertus@users.noreply.github.com>
2026-01-22 14:24:32 +0100

    fix: update Android file opening flags in ExpoFileViewerModule

--- src/ExpoFileViewerModule.ts
+++ src/ExpoFileViewerModule.ts
@@ -17,13 +17,10 @@ switch (Platform.OS) {
   case "android":
     openFile = async (filePath: string) => {
       const contentUri = await FileSystem.getContentUriAsync(filePath);
-      // Android Intent flags:
-      // FLAG_ACTIVITY_NEW_TASK = 0x10000000 = 268435456
       // FLAG_GRANT_READ_URI_PERMISSION = 0x00000001 = 1
-      // Combined: 268435456 | 1 = 268435457
       await startActivityAsync("android.intent.action.VIEW", {
         data: contentUri,
-        flags: 268435457, // FLAG_ACTIVITY_NEW_TASK | FLAG_GRANT_READ_URI_PERMISSION
+        flags: 1, // FLAG_GRANT_READ_URI_PERMISSION
       });
     };
     break;
